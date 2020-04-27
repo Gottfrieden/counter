cb012091294fc17d08052e0373489575d6d2fe49
Geoffroy <geoffroy.crabieres@gmail.com>
2020-04-27 14:08:54 +0200

    removed SelectCounter.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
-import SelectCounter from './SelectCounter';
 
 function App() {
   return (
